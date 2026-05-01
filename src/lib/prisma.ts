57a5d285e93fbf76fa95dbce6fcea4dcb38ec1c9
kenjivafe <kenjivafe@gmail.com>
2026-05-02 05:21:39 +0800

    fix: use pg adapter for PrismaClient to fix URL protocol error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,13 @@
 import { PrismaClient } from "../generated/prisma";
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = new PrismaClient({
-  accelerateUrl: process.env.DATABASE_URL
-});
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
