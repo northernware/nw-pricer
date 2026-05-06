a3fc574312cbcf65be6ca0dd2e227eb4084867c9
kenjivafe <kenjivafe@gmail.com>
2026-05-07 07:01:01 +0800

    ui: use plain plus icon for adding prospects

--- src/components/ClientBoard.tsx
+++ src/components/ClientBoard.tsx
@@ -100,7 +100,7 @@ export default function ClientBoard({ initialClients }: { initialClients: any[] 
                       className="ml-2 p-1 hover:text-nw-acid transition-colors"
                       title="Add Prospect"
                     >
-                      <Icon icon="solar:add-circle-linear" className="w-4 h-4" />
+                      <Icon icon="solar:add-linear" className="w-4 h-4" />
                     </button>
                   )}
                 </div>
